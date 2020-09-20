73504e2919e5da67b605661d7a61b9bfeef7ecb6
Sam Reinart <sam.reinart@gmail.com>
2020-09-20 12:42:32 -0500

    got sideways card list working

--- src/components/song_table/SongCard.js
+++ src/components/song_table/SongCard.js
@@ -2,21 +2,34 @@ import React from 'react';
 
 function SongCard(props) {
 	return (
-		<div className={'song-card zoom'}>
-			<div className={'song-card-container'}>
-				<div className={'song-card-rank'}>{ props.rank }</div>
-				<section className={'song-card-details'}>
-					<h2 className={'song-card-title'}>{ props.title }</h2>
-					<h3>{ props.artist }</h3>
-					<p>{ props.album }</p>
-				</section>
-				<img 
-					className={'song-card-cover'} 
-					src={props?.cover?.url} 
-					alt={'Album cover artwork'} 
-				/>
+		<article className={'song-card'}>
+			<header className={'song-card-header'}>
+				<p>{ props.rank }</p>
+				<h2>{ props.title }</h2>
+				<p>{ props.album }</p>
+			</header>
+			<div className={'song-card-details'}>
+				<a class="artist-avatar" href="#">
+					<img 
+						src={ props?.cover?.url } 
+						alt={'Album cover artwork'}
+					/>
+				</a>
+				<svg class="half-circle" viewBox="0 0 106 57">
+					<path d="M102 4c0 27.1-21.9 49-49 49S4 31.1 4 4"></path>
+				</svg>
+
+				<div class="artist-name">
+					<div class="artist-name-prefix">Artist</div>
+					{ props.artist }
+				</div>
 			</div>
-		</div>
+			<div class="tags">
+				<a href="#">Genre</a>
+				<a href="#">Genre</a>
+				<a href="#">Genre</a>
+			</div>
+		</article>
 	);
 }
 
--- src/components/song_table/SongList.js
+++ src/components/song_table/SongList.js
@@ -3,21 +3,21 @@ import SongCard from './SongCard';
 
 function SongTable(props) {
 	return (
-		<table className={'song-list'}>
-			<tbody>
-				{
-					props.titles.map((title, index) => {
-						return (
-							<tr key={title + index}>
-								<td>
-									<SongCard rank={index+1} title={title} artist={props.artists[index]} album={props.albums[index]} cover={props.covers[index]} />
-								</td>
-							</tr>
-						);
-					})
-				}
-			</tbody>
-		</table>
+		<section className={'song-list'}>
+			{
+				props.titles.map((title, index) => {
+					return (
+						<SongCard 
+							rank={index+1} 
+							title={title} 
+							artist={props.artists[index]} 
+							album={props.albums[index]} 
+							cover={props.covers[index]} 
+						/>
+					);
+				})
+			}
+		</section>
 	);
 }
 
